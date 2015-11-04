faf150ce4e91a56567db430d9c7f447a668d3c09
winrox <winnie.mabee@gmail.com>
2015-11-04 08:07:11 -0600

    #5 Scoreboard is now a function as well

--- src/js/components/Scoreboard.jsx
+++ src/js/components/Scoreboard.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
-export default class Scoreboard extends React.Component{
-  render() {
-    return(
-      <table className="col-sm-2">
-        <thead>
-          <tr>
-            <td colSpan="2">scoreboard</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><strong>X</strong></td>
-            <td><strong>O</strong></td>
-          </tr>
-          <tr>
-            <td><p id="x-score">{this.props.score.scoreX}</p></td>
-            <td><p id="o-score">{this.props.score.scoreO}</p></td>
-          </tr>
-        </tbody>
-      </table>
-    )
-  }
-};
+const Scoreboard = (props) => (
+  <table className="col-sm-2">
+    <thead>
+      <tr>
+        <td colSpan="2">scoreboard</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><strong>X</strong></td>
+        <td><strong>O</strong></td>
+      </tr>
+      <tr>
+        <td><p id="x-score">{props.score.scoreX}</p></td>
+        <td><p id="o-score">{props.score.scoreO}</p></td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+export default Scoreboard;
