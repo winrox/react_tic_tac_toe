a8ef75c4c0eb3c91509321ea090915f57ef25d1b
winrox <winnie.mabee@gmail.com>
2015-11-04 08:20:42 -0600

    #5 Header component too

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -4,15 +4,13 @@ import Scoreboard from './Scoreboard.jsx';
 import PlayAgain from './PlayAgain.jsx';
 import Title from './Title.jsx';
 
-export default class Header extends React.Component{
-  render() {
-    return(
-      <div className="page-header">
-        <Title />
-        <Scoreboard score={this.props.score} />
-        <PlayAgain clickCounter={this.props.clickCounter} foundWinner={this.props.foundWinner}/>
-        <Message message={this.props.message} />
-      </div>
-    )
-  }
-};
+const Header = (props) => (
+  <div className="page-header">
+    <Title />
+    <Scoreboard score={props.score} />
+    <PlayAgain clickCounter={props.clickCounter} foundWinner={props.foundWinner}/>
+    <Message message={props.message} />
+  </div>
+);
+
+export default Header;
